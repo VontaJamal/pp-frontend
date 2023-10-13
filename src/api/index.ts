ce6343b848c794433fe3815b90ef77a813bf4393
Vonta <djwstech@gmail.com>
2023-10-13 11:34:07 -0400

    add fetch functionality for user data.

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,8 +6,23 @@ Use the default fetch API.
 (Optional) Use JSDoc to document the functions.
 */
 
-export const me = () => {
+export const me = async (): Promise<any> => {
   // GET https://us-central1-internals-358114.cloudfunctions.net/react-challenge/me
+  const response = await fetch(
+    'https://us-central1-internals-358114.cloudfunctions.net/react-challenge/me',
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`,
+      },
+    }
+  );
+
+  if (response.ok) {
+    const userData = await response.json();
+    return userData;
+  } else {
+    throw new Error('Error retrieving user data');
+  }
 };
 
 export const phone = () => {
