26d4423110fba0b965c70449a2a5ab60281a0c33
Vonta <djwstech@gmail.com>
2023-10-13 12:00:50 -0400

    add UserProvider and display customer masked phone number, name, and email

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import * as Theme from '@theme';
+import {UserProvider, useUser} from '@providers';
+// import * as API from '@api';
 
 /* TODO:
 1. Center Card component within the #home.
@@ -7,13 +9,27 @@ import * as Theme from '@theme';
 3. Display user's masked phone number, and implement a way to unmask it. Use UserProvider's context.
 */
 
+const phoneStyle = {
+  display: 'flex',
+};
+
 const Card = () => {
+  const {user} = useUser();
+
+  if (!user) return;
+  const {first_name, last_name, email, masked_phone} = user;
+
   return (
-    <div id="card">
-      ...
+    <div id='card'>
+      <p>{`${first_name} ${last_name}`}</p>
+      <p>{`${email}`}</p>
+      <div style={phoneStyle}>
+        <p>{`${masked_phone}`}</p>
+        <button onClick={() => {}}>Unmask Phone Number</button>
+      </div>
     </div>
-  ); 
-}
+  );
+};
 
 const homeStyle = {
   backgroundColor: Theme.colors.beige,
@@ -23,10 +39,12 @@ const homeStyle = {
 
 const Home = () => {
   return (
-    <div id="home" style={homeStyle}>
-      <Card />
-    </div>
+    <UserProvider>
+      <div id='home' style={homeStyle}>
+        <Card />
+      </div>
+    </UserProvider>
   );
-}
+};
 
 export default Home;
